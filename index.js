d993af391c30f0873698cbac209c53965d21c7b9
Amanda <amacrisgomes@hotmail.com>
2019-07-05 17:12:22 -0300

    add swagger

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const db = require('./models/index');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 app.use(express.json());
 
@@ -12,9 +14,9 @@ app.get('/', (req, resp) => {
   resp.send('Bem vindo à API Burger Queen!');
 })
 
-app.use('/users', require('./routes/users'))
-app.use('/products', require('./routes/products'))
-app.use('/orders', require('./routes/orders'))
-
+app.use('/users', require('./routes/users'));
+app.use('/products', require('./routes/products'));
+app.use('/orders', require('./routes/orders'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 db.sequelize.sync();
